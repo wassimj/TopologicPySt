1c25db176746a5a50cb03a10d2372ead551ba187
Wassim Jabi <jabiw@cardiff.ac.uk>
2022-09-28 08:25:00 +0100

    Debugging

--- src/topologicpy/__init__.py
+++ src/topologicpy/__init__.py
@@ -9,6 +9,7 @@ else:
     os_name = 'linux'
 
 sitePackagesFolderName = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bin", os_name)
+print(os.listdir(sitePackagesFolderName))
 topologicFolderName = [filename for filename in os.listdir(sitePackagesFolderName) if filename.startswith("topologic")][0]
 topologicPath = os.path.join(sitePackagesFolderName, topologicFolderName)
 sys.path.append(topologicPath)

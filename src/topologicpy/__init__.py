dc7505eb24175158cde06a44c27a585add0d6acd
Wassim Jabi <jabiw@cardiff.ac.uk>
2022-09-28 08:14:55 +0100

    Update __init__.py

--- src/topologicpy/__init__.py
+++ src/topologicpy/__init__.py
@@ -3,17 +3,6 @@ import sys
 import os, re
 from sys import platform
 
-
-#sitePackagesFolderName = os.path.join(os.path.dirname(os.path.realpath(__file__)), "site-packages")
-#topologicFolderName = [filename for filename in os.listdir(sitePackagesFolderName) if filename.startswith("topologic")][0]
-#topologicPath = os.path.join(sitePackagesFolderName, topologicFolderName)
-#sys.path.append(topologicPath)
-#topologicPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "site-packages")
-#sys.path.append(topologicPath)
-import sys
-import os, re
-from sys import platform
-
 if platform == 'win32':
     os_name = 'windows'
 else:
@@ -22,8 +11,6 @@ else:
 sitePackagesFolderName = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bin", os_name)
 topologicFolderName = [filename for filename in os.listdir(sitePackagesFolderName) if filename.startswith("topologic")][0]
 topologicPath = os.path.join(sitePackagesFolderName, topologicFolderName)
-sys.path.append(topologicPath)
-topologicPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "site-packages")
 sys.path.append(topologicPath)
 
 import topologic
